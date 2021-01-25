ed62e098e08cfd0df7cac9fd99f32be17066dd62
66maer <505670805@qq.com>
2021-01-25 18:16:03 +0800

    临时保存，回家写

--- src/TeamModule.h
+++ src/TeamModule.h
@@ -9,4 +9,9 @@ public:
 
 private:
     bool Init();
+
+    bool Trigger(const std::string& mess, const std::vector<std::string>& keys, bool first = true);
+
+    void CreateTeamProc(const GroupMessageData& data);
+
 };
